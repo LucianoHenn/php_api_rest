6363b73b77888c6c04eb419e563a2c5bb3ada458
LucianoHenn <79486565+LucianoHenn@users.noreply.github.com>
2022-06-12 18:07:45 -0300

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "o0BDB(Qz9056OurB");
+define("DB_PASSWORD", "YOUR_DB_PASSWORD");
 define("DB_DATABASE_NAME", "rest_api_demo");
